4011761826df2f247a5d77e61a413efe2cab88ec
Mose Kim <100126792+MoseKim1214@users.noreply.github.com>
2023-07-25 11:50:41 +0900

    Mysite04 WebConfig I

---
+++ mysite04/src/main/java/com/bitacademy/mysite/config/web/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.bitacademy.mysite.config.web;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.bitacademy.mysite.security.AuthInterceptor;
+import com.bitacademy.mysite.security.AuthUserHandlerMethodArgumentResolver;
+import com.bitacademy.mysite.security.LoginInterceptor;
+import com.bitacademy.mysite.security.LogoutInterceptor;
+
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer {
+	
+	
+	//Argument Resolver
+	@Bean
+	public  HandlerMethodArgumentResolver handlerMethodArgumentResolver() {
+		return new AuthUserHandlerMethodArgumentResolver();
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(handlerMethodArgumentResolver());
+	}
+
+	
+	
+	//Interceptors
+	
+	
+	public HandlerInterceptor authInterceptor() {
+		return new AuthInterceptor();
+	}
+	
+	public HandlerInterceptor loginInterceptor() {
+		return new LoginInterceptor();
+	}
+	
+
+
+	public HandlerInterceptor logoutInterceptor() {
+		return new LogoutInterceptor();
+	}
+	
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(loginInterceptor())
+				.addPathPatterns("/user/auth");
+	}
+}
